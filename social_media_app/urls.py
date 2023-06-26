c155e41572131098f980333a0c9bb0209ebc35ff
Yash <jewalkaryash@gmail.com>
2023-06-26 09:42:34 +0530

    added signup feature

--- social_media_app/urls.py
+++ social_media_app/urls.py
@@ -1,8 +1,9 @@
 from social_media_app.views import index_view, sign_up_view, sign_in_view
 from django.urls import path
 urlpatterns = [
-    path('',index_view),
-    path('sign_up/',sign_up_view),
-    path('sign_in/',sign_in_view),
+    path('',index_view ,name="index"),
+    path('sign-up/',sign_up_view,name="sign_up"),
+    path('sign-in/',sign_in_view,name="sign_in"),
 
 ]
+
